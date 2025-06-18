fb334db68aaf45e2612316e94b8b0215fea5c8c0
Kimmo <kimmolaine1@kamk.fi>
2025-06-18 08:21:38 +0300

    services

---
+++ start_services.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+# source /home/kipe/biljardivaraus/.venv/bin/activate
+
+# echo "Käynnistetään: lomake"
+# streamlit run /home/kipe/biljardivaraus/pages/01_lomake.py --server.port 8501>
+
+# echo "Käynnistetään: varaukset"
+# streamlit run /home/kipe/biljardivaraus/pages/02_varaukset.py --server.port 8>
+
+# echo "Käynnistetään: ohjaaja"
+# streamlit run /home/kipe/biljardivaraus/pages/03_ohjaaja.py --server.port 850>
+
+# echo "Käynnistetään: qr"
+# streamlit run /home/kipe/biljardivaraus/pages/04_qr.py --server.port 8504 &
+
+echo "Käynnistetään: main"
+uv run streamlit run /home/kipe/biljardivaraus/main.py --client.showSidebarNavi>
+
---
+++ stop_services.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+# Etsi ja lopeta kaikki pyörivät uv-prosessit
+echo "Sammutetaan palvelut..."
+
+# Etsi kaikki uv-prosessit ja tapa ne
+pkill -f 'uv run'
+
+echo "Kaikki palvelut on sammutettu."
+
+
+
+
+
+
+
+
+
