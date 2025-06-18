fd98443af2a104407a67235aecbf52d925d5006d
Kimmo <kimmolaine1@kamk.fi>
2025-06-18 08:27:26 +0300

    muokattu start

--- start_services.sh
+++ start_services.sh
@@ -4,13 +4,13 @@
 # source /home/kipe/biljardivaraus/.venv/bin/activate
 
 # echo "Käynnistetään: lomake"
-# streamlit run /home/kipe/biljardivaraus/pages/01_lomake.py --server.port 8501>
+# streamlit run /home/kipe/biljardivaraus/pages/01_lomake.py --server.port 8501 &
 
 # echo "Käynnistetään: varaukset"
-# streamlit run /home/kipe/biljardivaraus/pages/02_varaukset.py --server.port 8>
+# streamlit run /home/kipe/biljardivaraus/pages/02_varaukset.py --server.port 8502 &
 
 # echo "Käynnistetään: ohjaaja"
-# streamlit run /home/kipe/biljardivaraus/pages/03_ohjaaja.py --server.port 850>
+# streamlit run /home/kipe/biljardivaraus/pages/03_ohjaaja.py --server.port 8503 &
 
 # echo "Käynnistetään: qr"
 # streamlit run /home/kipe/biljardivaraus/pages/04_qr.py --server.port 8504 &
